1cc926c113d9f4c623768d2b4722f9adfb0db30c
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-04-21 17:03:08 -0300

    gfn

--- javascript.js
+++ javascript.js
@@ -61,3 +61,14 @@ function zoomImg(posicao) {
     let modal = new bootstrap.Modal(document.getElementById('modalZoom'));
     modal.show();
 }
+
+function alterarTemaSite() {
+    let tema = document.querySelector('html').getAttribute('data-bs-theme');
+    if (tema === 'dark') {
+        document.querySelector('html').setAttribute('data-bs-theme', 'light');
+        document.querySelector('#alterarTemaSite').innerHTML = '<i class="bi bi-moon-stars-fill"></i>';
+    } else {
+        document.querySelector('html').setAttribute('data-bs-theme', 'dark');
+        document.querySelector('#alterarTemaSite').innerHTML = '<i class="bi bi-brightness-high"></i>';
+    }
+}
